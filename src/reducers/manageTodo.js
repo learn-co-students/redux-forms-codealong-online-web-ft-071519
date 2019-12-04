8a10ca159f5ec723cdef2c93a7aaeb0e623e0d73
github username <github email address>
2019-12-04 11:50:31 +0000

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -2,7 +2,17 @@ export default function manageTodo(state = {
   todos: [],
 }, action) {
 
-  console.log("reducer received this action:", action);
+  console.log(state)
+  
 
-  return state;
+  switch (action.type){
+    case "ADD_TODO":
+        console.log("reducer received this action:", action);
+        console.log(action.payload)
+        console.log({ todos: state.todos.concat(action.payload.text) });
+        return { todos: state.todos.concat(action.payload.text)}
+    default:
+      return state
+
+  }
 }
